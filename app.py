eaa470b59b53f5715b078d36249fc453bf072ed9
huan0925 <62659479+huan0925@users.noreply.github.com>
2024-10-08 23:48:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,25 +6,27 @@ from openai import OpenAI
 from composio_openai import ComposioToolSet, App
 from composio.client.exceptions import NoItemsFound
  
-def run_composio(msg, Davinci_API_KEY, Composio_API_KEY, Entity_ID):
+def run_composio(msg, Davinci_API_KEY, Composio_API_KEY, Entity_ID, App_list):
     # 初始化 OpenAI 客戶端
     openai_client = OpenAI(
-        base_url = "https://davinci.mediatek.inc/api/assts/v1",
+        base_url = "https://prod.dvcbot.net/api/assts/v1",
         api_key = Davinci_API_KEY
     )
 
     # 初始化 Composio 工具集
-
-    # Youtube
-    # composio_toolset = ComposioToolSet(api_key="nz8dbhjwoibd3iee6l45b", entity_id="default")
-    #Gmail
     composio_toolset = ComposioToolSet(api_key=Composio_API_KEY, entity_id=Entity_ID)
    
     # 步驟 3：通過 Composio 獲取所有 GitHub 工具
-    app_enum = getattr(App, 'GMAIL'.upper(), None)
-    if app_enum is None:
-        raise ValueError(f'Invalid name')
-    tool = composio_toolset.get_tools(apps=[app_enum])
+    App_list_attr = []
+    
+    for app in App_list:
+        # print(app)
+        app_enum = getattr(App, app.upper(), None)
+        if app_enum is None:
+            raise ValueError(f'Invalid name')
+        else:
+            App_list_attr.append(app_enum)
+    tool = composio_toolset.get_tools(apps=App_list_attr)
     # tool = composio_toolset.get_tools(apps=[App.YOUTUBE, App.GMAIL])
     # tool = composio_toolset.get_tools(apps=[App.GMAIL, App.YOUTUBE])
     my_task = msg # 在達哥輸入的內容
@@ -43,9 +45,9 @@ def run_composio(msg, Davinci_API_KEY, Composio_API_KEY, Entity_ID):
     # create a thread
     thread = openai_client.beta.threads.create()
     message = openai_client.beta.threads.messages.create(thread_id=thread.id,role="user",content=my_task)
-
+    
     # Execute Agent with integrations
-    run = openai_client.beta.threads.runs.create(thread_id=thread.id,assistant_id=assistant.id)
+    run = openai_client.beta.threads.runs.create(thread_id=thread.id,assistant_id=assistant.id) # choose action
     response_after_tool_calls = composio_toolset.wait_and_handle_assistant_tool_calls(
         client=openai_client,
         run=run,
@@ -64,8 +66,7 @@ def run_composio(msg, Davinci_API_KEY, Composio_API_KEY, Entity_ID):
         messages = openai_client.beta.threads.messages.list(thread_id=thread.id).data
         print(messages[0])
     
-    return messages[0].content[0].text.value
-
+    return messages[0].content[0].text.value   
 
 app = Flask(__name__)
 # Enable CORS for all domains on all routes
@@ -77,12 +78,14 @@ def home():
     headers={
       "Content-Type":"application/json"
     }
+    print(data)
     msg = data['conversation'][-1]['content']
     Davinci_API_KEY = data['Davinci_API_KEY']
     Composio_API_KEY = data['Composio_API_KEY']
     Entity_ID = data['Entity_ID']
+    App_list = data['App']
     # print(msg)
-    response = run_composio(msg, Davinci_API_KEY, Composio_API_KEY, Entity_ID)
+    response = run_composio(msg, Davinci_API_KEY, Composio_API_KEY, Entity_ID, App_list)
     # print(response)
     response_data = {"response": response}
     
